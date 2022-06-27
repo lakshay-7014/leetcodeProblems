0eb807a481d4c9dc19823a3c4052977f8e1d4844
Lakshay Agarwal <73326673+lakshay-7014@users.noreply.github.com>
2022-06-27 22:46:53 +0530

    Update 137-single-number-ii.cpp

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int i=0;
+        while(i<n){
+            if(i<n-2 && nums[i]==nums[i+1] && nums[i+1]==nums[i+2]){
+                i=i+2;
+            }
+            else{
+                return nums[i];
+            }
+            i++;
+        }
+        return -1;
+    }
+};
+
+//Bit Manipulation 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
         int n=nums.size();
         int ones=0;
         int twos=0;
