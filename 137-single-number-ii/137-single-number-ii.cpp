4823c3d9fcce8a6aec69f4b0680c099f1654e2f2
Lakshay Agarwal <73326673+lakshay-7014@users.noreply.github.com>
2022-06-27 22:44:22 +0530

    Time: 5 ms (94.79%), Space: 9.6 MB (56.04%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int n=nums.size();
-        int i=0;
-        while(i<n){
-            if(i<n-2 && nums[i]==nums[i+1] && nums[i+1]==nums[i+2]){
-                i=i+2;
-            }
-            else{
-                return nums[i];
-            }
-            i++;
+        int ones=0;
+        int twos=0;
+        for(int i=0;i<n;i++){
+            ones=ones^nums[i];
+            ones=ones& ~(twos);
+            
+            twos=twos^nums[i];
+            twos=twos& ~(ones);
         }
-        return -1;
+        return ones;
     }
 };
