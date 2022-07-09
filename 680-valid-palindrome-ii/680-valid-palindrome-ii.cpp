9a920b5558c2663ea18e20bb3c7ad06eeddb601e
Lakshay Agarwal <73326673+lakshay-7014@users.noreply.github.com>
2022-07-09 10:39:29 +0530

    Time: 94 ms (26.22%), Space: 19.6 MB (76.09%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int st=0;
+        int e=s.length()-1;
+        int flag=0;
+        int flag1=0;
+        while(st<e){
+            if(flag==0 && s[st]!=s[e]){
+                flag=1;
+                st++;
+                // if(s[st]!=s[e]){
+                //     st--;
+                //     e--;
+                // }
+            }
+            else if(s[st]!=s[e]){
+                flag=2;
+                break;
+            }
+            else{
+                //swap(s[st],s[e]);
+                st++;
+                e--;
+            }
+        }
+        st=0;
+        e=s.length()-1;
+        while(flag==2 && st<e){
+            if(flag1==0 && s[st]!=s[e]){
+                flag1=1;
+                e--;
+                // if(s[st]!=s[e]){
+                //     st--;
+                //     e--;
+                // }
+            }
+            else if(s[st]!=s[e]){
+                return false;
+            }
+            else{
+                swap(s[st],s[e]);
+                st++;
+                e--;
+            }
+        }
+        return true;
+    }
+};
