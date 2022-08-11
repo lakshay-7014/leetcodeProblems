2f1d073b62f36a488d471671ead00d55ef5c9174
Lakshay Agarwal <73326673+lakshay-7014@users.noreply.github.com>
2022-08-11 12:59:52 +0530

    Time: 10 ms (78.86%), Space: 6 MB (64.52%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,33 +1,38 @@
+int value(char c){
+    if(c=='I'){
+        return 1;
+    }else if(c=='V'){
+        return 5;
+    }else if(c=='X'){
+        return 10;
+    }else if(c=='L'){
+        return 50;
+    }else if(c=='C'){
+        return 100;
+    }else if(c=='D'){
+        return 500;
+    }else if(c=='M'){
+        return 1000;
+    }
+    return 0;
+}
+
 class Solution {
 public:
     int romanToInt(string s) {
-        map<char,int> ans;
-        // insert elements in random order
-    ans.insert(pair<char, int>('I', 1));
-    ans.insert(pair<char, int>('V', 5));
-    ans.insert(pair<char, int>('X', 10));
-    ans.insert(pair<char, int>('L', 50));
-    ans.insert(pair<char, int>('C', 100));
-    ans.insert(pair<char, int>('D', 500));
-    ans.insert(pair<char, int>('M', 1000));
-        int sum=0;
+        vector<int>v;
+        int ans=0;
         int i=0;
-        int size=s.length();
-        while(i<=size-1){
-            if(ans[s[i]]>=ans[s[i+1]]){
-                sum+=ans[s[i]];
+        while(s[i]!='\0'){
+            if(value(s[i])>=value(s[i+1])){
+                ans+=value(s[i]);
+            }else{
+                ans-=value(s[i]);
             }
-            else{
-                sum+= ans[s[i+1]]-ans[s[i]];
-                i++;
-            }
-           // cout<<sum<<" ";
             i++;
+            
         }
-        if(i<size-1){
-            sum+=ans[s[i]];
-            i++;
-        }
-        return sum;
+        
+        return ans;
     }
 };
