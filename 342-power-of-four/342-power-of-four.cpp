cfbdfec9f0cc571979b60777e2c9418ab1daf079
Lakshay Agarwal <73326673+lakshay-7014@users.noreply.github.com>
2022-08-26 22:48:42 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.97%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==0){
+            return false;
+        }
+        while(1){
+            if(n==1){
+                return true;
+            }
+            if(n%4!=0){
+                return false;
+            }
+            n=n/4;
+        }
+        return true;
+    }
+};
