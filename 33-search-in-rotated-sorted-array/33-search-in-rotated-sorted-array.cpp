cc2c4c309bbd001c085b3a4d488fd45ebe6e2406
Lakshay Agarwal <73326673+lakshay-7014@users.noreply.github.com>
2022-09-11 13:26:14 +0530

    Time: 9 ms (32.26%), Space: 11.1 MB (75.00%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,32 +1,48 @@
 class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int s=0;
-        int e=nums.size()-1;
+    private:
+     int getpivot(vector<int>& arr){
+         int s=0;
+         int e=arr.size()-1;
+         int mid=s+(e-s)/2;
+         while(s<e){
+             if(arr[0]<=arr[mid]){
+                 s=mid+1;
+             }
+             else{
+                 e=mid;
+             }
+             mid=s+(e-s)/2;
+         }
+         return mid;
+     }
+    int binarysearch(vector<int>& nums,int s,int e,int target){
         int mid=s+(e-s)/2;
         while(s<=e){
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[mid]>=nums[s]){ //left arrray sorted hai
-               if(target >=nums[s] && target <=nums[mid]){
-                   e=mid;
-               }
-                else{
-                    s=mid+1;
-                }
-                
+            else if(nums[mid]>target){
+                e=mid-1;
             }
-            else if(nums[mid]<=nums[s]){
-                if(target>=nums[mid] && target<=nums[e]){
-                    s=mid+1;
-                }
-                else{
-                    e=mid;
-                }
+            else{
+                s=mid+1;
             }
             mid=s+(e-s)/2;
         }
         return -1;
+    }
+    
+public:
+    
+    int search(vector<int>& nums, int target) {
+        int pivot= getpivot(nums);
+        int n=nums.size()-1;
+        if(target>=nums[pivot] && target<=nums[n]){
+            return binarysearch(nums,pivot,n,target);
+        }
+        else{
+            return binarysearch(nums,0,pivot-1,target);
+        }
+        return 0;
     }
 };
