bb4209ab9dacdb3168d9811d128938d6bde62876
Lakshay Agarwal <73326673+lakshay-7014@users.noreply.github.com>
2022-10-31 21:13:45 +0530

    Time: 248 ms (5.12%), Space: 47.4 MB (5.05%) - LeetHub

---
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int hardestWorker(int n, vector<vector<int>>& logs) {
+        map<int,int> mp1;
+        map<int,int> mp2;
+        int row= logs.size();
+        int s=0;
+        int maxi=0;
+        int ans =INT_MAX;
+        for(int i=0;i<row;i++){
+            int e= logs[i][1];
+            mp1[i]= e-s;
+            s=e;
+        }
+        for(int i=0;i<row;i++){
+            mp2[i]=logs[i][0];
+        }
+        
+        for(int i=0;i<row;i++){
+            int val= mp1[i];
+            maxi=max(maxi,val);
+            
+        }
+        for(int i=0;i<row;i++){
+            if(mp1[i]==maxi){
+                int id = mp2[i];
+                ans=min(ans,id);
+            }
+        }
+        return ans;
+        
+    }
+};
