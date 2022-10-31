0577dc187ae27d0ce316458dde5c3e5e7a880ead
Lakshay Agarwal <73326673+lakshay-7014@users.noreply.github.com>
2022-10-31 23:00:52 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (73.85%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int count=1;
+        
+        int n = max(a,b);
+        for(int i=2;i<=n;i++){
+            if((a%i)==0  && (b%i)==0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
