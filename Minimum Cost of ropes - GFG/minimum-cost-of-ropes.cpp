3e494d8c95f44312b7f189e8eeb238bccc9f2378
Lakshay Agarwal <73326673+lakshay-7014@users.noreply.github.com>
2023-01-28 01:26:13 +0530

    Added solution - LeetHub

--- Minimum Cost of ropes - GFG/minimum-cost-of-ropes.cpp
+++ Minimum Cost of ropes - GFG/minimum-cost-of-ropes.cpp
@@ -10,19 +10,18 @@ class Solution
     //Function to return the minimum cost of connecting the ropes.
     long long minCost(long long arr[], long long n) {
         // Your code here
-        priority_queue<long long , vector<long long > ,greater<long long> > pq;
-        for(int i=0 ; i<n;i++){
+        priority_queue<long long , vector<long long> ,greater<long long>> pq;
+        long long cost=0;
+        for(int i=0;i<n;i++){
             pq.push(arr[i]);
         }
-        long long cost=0;
         while(pq.size()>1){
-            long long a =pq.top();
+            long long f1=pq.top();
             pq.pop();
-            long long b =pq.top();
+            long long f2=pq.top();
             pq.pop();
-            long long sum = a+b;
-            cost+=sum;
-            pq.push(sum);
+            cost=cost+ f1+f2;
+            pq.push(f1+f2);
         }
         return cost;
     }
